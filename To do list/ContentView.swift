3fed9b7b950641e041d6e965d16e8ad42ec40528
Morgan Rees <morgan.w.rees@gmail.com>
2021-01-29 19:27:40 -0600

    work

--- To do list/ContentView.swift
+++ To do list/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+        List {
+      Text("First thing")
+            Text("Second thing")
+            Text("Three thing")
+            Text("Fourth thing")
+            Text("Fifth thing")
+        }
+        .navigationBarTitle("Things")
+        }
     }
-}
+    }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
